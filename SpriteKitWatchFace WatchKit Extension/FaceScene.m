d6b2cece03ece8b03e942523508407996cb04699
Dimitri Bouniol <Dimitri.Bouniol@efilm.com>
2018-10-11 20:19:36 -0700

    Actually, go ahead and throw some math at the problem

--- SpriteKitWatchFace WatchKit Extension/FaceScene.m
+++ SpriteKitWatchFace WatchKit Extension/FaceScene.m
@@ -12,6 +12,191 @@
 #define NSFont UIFont
 #endif
 
+// Adapted from https://www.particleincell.com/2013/cubic-line-intersection/
+NSArray<NSValue *> *intersectionBetweenCubicCurveAndLine(CGPoint curvePointA, CGPoint curvePointB, CGPoint curvePointC, CGPoint curvePointD, CGPoint linePointA, CGPoint linePointB)
+{
+    CGFloat xAmB = linePointA.x - linePointB.x;
+    CGFloat xBmA = linePointB.x - linePointA.x;
+    CGFloat yAmB = linePointA.y - linePointB.y;
+    CGFloat yBmA = linePointB.y - linePointA.y;
+    CGFloat lineConstant = linePointA.x*yAmB + linePointA.y*xBmA;
+    
+    
+    CGPoint bezierCoefficient0 = CGPointMake(-curvePointA.x + 3.*curvePointB.x - 3.*curvePointC.x + curvePointD.x,
+                                             -curvePointA.y + 3.*curvePointB.y - 3.*curvePointC.y + curvePointD.y);
+    
+    CGPoint bezierCoefficient1 = CGPointMake(3.*curvePointA.x - 6.*curvePointB.x + 3.*curvePointC.x,
+                                             3.*curvePointA.y - 6.*curvePointB.y + 3.*curvePointC.y);
+    
+    CGPoint bezierCoefficient2 = CGPointMake(-3.*curvePointA.x + 3.*curvePointB.x,
+                                             -3.*curvePointA.y + 3.*curvePointB.y);
+    
+    CGPoint bezierCoefficient3 = CGPointMake(curvePointA.x,
+                                             curvePointA.y);
+    
+    CGFloat polynomialCoefficient0 = yBmA*bezierCoefficient0.x + xAmB*bezierCoefficient0.y; // t^3
+    CGFloat polynomialCoefficient1 = yBmA*bezierCoefficient1.x + xAmB*bezierCoefficient1.y; // t^2
+    CGFloat polynomialCoefficient2 = yBmA*bezierCoefficient2.x + xAmB*bezierCoefficient2.y; // t^1
+    CGFloat polynomialCoefficient3 = yBmA*bezierCoefficient3.x + xAmB*bezierCoefficient3.y + lineConstant; // t^0
+    
+    if (polynomialCoefficient0 == 0) return nil;
+    
+    CGFloat A = polynomialCoefficient1/polynomialCoefficient0;
+    CGFloat B = polynomialCoefficient2/polynomialCoefficient0;
+    CGFloat C = polynomialCoefficient3/polynomialCoefficient0;
+    
+    CGFloat Q = (3.*B - A*A)/9.;
+    CGFloat R = (9.*A*B - 27*C - 2.*A*A*A)/54.;
+    
+    CGFloat discriminant = Q*Q*Q + R*R;
+    
+    // Possible roots
+    CGFloat root0 = -1;
+    CGFloat root1 = -1;
+    CGFloat root2 = -1;
+    
+    if (discriminant >= 0) { // Complex or duplicate roots
+        CGFloat S = ((R + sqrt(discriminant)) < 0 ? -1. : 1.)*pow(ABS(R + sqrt(discriminant)), 1./3.);
+        CGFloat T = ((R - sqrt(discriminant)) < 0 ? -1. : 1.)*pow(ABS(R - sqrt(discriminant)), 1./3.);
+        
+        root0 = -A/3. + (S + T); // Real root
+        root1 = -A/3. - (S + T)/2.; // real part of complex root
+        root2 = -A/3. - (S + T)/2.; // real part of other complex root
+        CGFloat complexComponent = ABS(sqrt(3.)*(S - T)/2.);
+        
+        if (complexComponent) { // We are uninterested in complex roots
+            root1 = -1;
+            root2 = -1;
+        }
+    } else { // distinct real roots
+        CGFloat theta = acos(R/sqrt(-Q*Q*Q));
+        
+        root0 = 2.*sqrt(-Q)*cos(theta/3.) - A/3.;
+        root1 = 2.*sqrt(-Q)*cos((theta + 2.*M_PI)/3.) - A/3.;
+        root2 = 2.*sqrt(-Q)*cos((theta + 4.*M_PI)/3.) - A/3.;
+    }
+    
+    NSMutableArray<NSNumber *> *roots = [[NSMutableArray alloc] init];
+    
+    if (root0 >= 0 && root0 <= 1) [roots addObject:@(root0)];
+    if (root1 >= 0 && root1 <= 1) [roots addObject:@(root1)];
+    if (root2 >= 0 && root2 <= 1) [roots addObject:@(root2)];
+    
+    NSMutableArray<NSValue *> *intersections = [[NSMutableArray alloc] init];
+    
+    for (NSNumber *root in roots) {
+        CGFloat curveTime = root.doubleValue;
+        CGFloat t = curveTime;
+        
+        CGPoint intersectionPoint = CGPointMake(t*t*t*bezierCoefficient0.x + t*t*bezierCoefficient1.x + t*bezierCoefficient2.x + bezierCoefficient3.x,
+                                                t*t*t*bezierCoefficient0.y + t*t*bezierCoefficient1.y + t*bezierCoefficient2.y + bezierCoefficient3.y);
+        
+        CGFloat lineTime = 0;
+        
+        if (xBmA) {
+            lineTime = (intersectionPoint.x - linePointA.x)/xBmA;
+        } else {
+            lineTime = (intersectionPoint.y - linePointA.y)/yBmA;
+        }
+    
+        if (curveTime >= 0 && curveTime <= 1. && lineTime >= 0. && lineTime <= 1.)  {
+            [intersections addObject:[NSValue valueWithPoint:intersectionPoint]];
+        }
+    }
+    
+    return intersections;
+}
+
+// Adapted from https://stackoverflow.com/a/565282/1565236
+NSValue *intersectionBetweenLineAndLine(CGPoint line1PointA, CGPoint line1PointB, CGPoint line2PointA, CGPoint line2PointB)
+{
+    line1PointB.x -= line1PointA.x;
+    line1PointB.y -= line1PointA.y;
+    line2PointB.x -= line2PointA.x;
+    line2PointB.y -= line2PointA.y;
+    
+    CGFloat denomenator = line1PointB.x*line2PointB.y - line1PointB.y*line2PointB.x;
+    
+    if (denomenator == 0) return nil; // lines are parallel/colinear // handle case when line1 is a point here? Or maybe not necessary
+    
+    CGPoint difference = CGPointMake(line2PointA.x - line1PointA.x,
+                                   line2PointA.y - line1PointA.y);
+    
+    
+    CGFloat line1Time = (difference.x*line2PointB.y - difference.y*line2PointB.x)/denomenator;
+    CGFloat line2Time = (difference.x*line1PointB.y - difference.y*line1PointB.x)/denomenator;
+    
+    if (line1Time >= 0. && line1Time <= 1. && line2Time >= 0. && line2Time <= 1.) {
+        CGPoint intersection = CGPointMake(line1PointA.x + line1Time*line1PointB.x,
+                                           line1PointA.y + line1Time*line1PointB.y);
+        
+        return [NSValue valueWithPoint:intersection];
+    } else {
+        return nil;
+    }
+
+    return nil;
+}
+
+NSArray<NSValue *> *intersectionsBetweenPathAndLinePassingThroughPoints(CGPathRef path, CGPoint linePointA, CGPoint linePointB)
+{
+    NSMutableArray *intersections = [[NSMutableArray alloc] init];
+    
+    __block CGPoint firstPoint;
+    __block CGPoint lastPoint;
+    
+    CGPathApplyWithBlock(path, ^(const CGPathElement * _Nonnull elementPointer) {
+        CGPathElement element = *elementPointer;
+        
+        if (element.type == kCGPathElementMoveToPoint) {
+            firstPoint = element.points[0];
+            lastPoint = element.points[0];
+        } else if (element.type == kCGPathElementAddLineToPoint) {
+            // get intersection between both lines
+            
+            NSValue *localIntersection = intersectionBetweenLineAndLine(lastPoint, element.points[0], linePointA, linePointB);
+            if (localIntersection) [intersections addObject:localIntersection];
+            
+            lastPoint = element.points[0];
+        } else if (element.type == kCGPathElementAddQuadCurveToPoint) {
+            // get intersection between line and quad curve
+            
+            // cheat for now
+            NSValue *localIntersection = intersectionBetweenLineAndLine(lastPoint, element.points[1], linePointA, linePointB);
+            if (localIntersection) [intersections addObject:localIntersection];
+            
+            lastPoint = element.points[1];
+        } else if (element.type == kCGPathElementAddCurveToPoint) {
+            // get intersection between line and cubic curve
+            
+            NSArray *localIntersections = intersectionBetweenCubicCurveAndLine(lastPoint, element.points[0], element.points[1], element.points[2], linePointA, linePointB);
+            if (localIntersections.count) [intersections addObjectsFromArray:localIntersections];
+            
+            lastPoint = element.points[2];
+        } else if (element.type == kCGPathElementCloseSubpath) {
+            // get intersection between both lines
+            
+            NSValue *localIntersection = intersectionBetweenLineAndLine(lastPoint, firstPoint, linePointA, linePointB);
+            if (localIntersection) [intersections addObject:localIntersection];
+            
+            lastPoint = firstPoint;
+        }
+    });
+    
+    if (intersections.count == 0) {
+        NSLog(@"No intersections for %@ - %@", NSStringFromPoint(linePointA), NSStringFromPoint(linePointB));
+    }
+    
+    return intersections;
+}
+
+CGPoint intersectionBetweenPathAndLinePassingThroughPoints(CGPathRef path, CGPoint linePointA, CGPoint linePointB)
+{
+    NSArray<NSValue *> *intersections = intersectionsBetweenPathAndLinePassingThroughPoints(path, linePointA, linePointB);
+    
+    return intersections.firstObject.pointValue;
+}
+
 @implementation FaceScene
 
 - (instancetype)initWithCoder:(NSCoder *)coder
@@ -94,65 +279,88 @@
 	CGFloat labelXMargin = 24.0;
 
 	CGSize faceSize = (CGSize){184, 224};
-    CGFloat cornerRadius = 16;
+    CGFloat cornerRadius = 34;
     
-    CGFloat workingRadius = faceSize.width/2;
-    CGFloat longTickHeight = workingRadius/10.;
-    CGFloat shortTickHeight = workingRadius/20.;
+    CGFloat workingRadius = sqrt(faceSize.width/2.*faceSize.width/2. + faceSize.height/2.*faceSize.height/2.);
+    CGFloat longTickHeight = round(faceSize.width/2./10.);
+    CGFloat shortTickHeight = longTickHeight/2.;
     
-    /* Major */
-    SKCropNode *majorTicksLayer = [[SKCropNode alloc] init];
-    majorTicksLayer.position = CGPointZero;
-    majorTicksLayer.zPosition = 0;
+    CGPathRef outerPath = CGPathCreateWithRoundedRect(CGRectMake(margin - faceSize.width/2., margin - faceSize.height/2., faceSize.width - margin*2., faceSize.height - margin*2.), cornerRadius - margin, cornerRadius - margin, NULL);
     
-    SKShapeNode *majorTicksMask = [SKShapeNode shapeNodeWithRectOfSize:CGSizeMake(faceSize.width - margin*2. - longTickHeight, faceSize.height - margin*2. - longTickHeight) cornerRadius:cornerRadius - margin - longTickHeight/2.];
-    majorTicksMask.lineWidth = longTickHeight;
-    majorTicksMask.strokeColor = [SKColor colorWithWhite:1 alpha:1];
-    majorTicksMask.position = CGPointZero;
-    majorTicksMask.antialiased = NO;
-    
-    majorTicksLayer.maskNode = majorTicksMask;
-    [self addChild:majorTicksLayer];
+    CGPathRef largeTickInnerPath = CGPathCreateWithRoundedRect(CGRectMake(margin + longTickHeight - faceSize.width/2., margin + longTickHeight - faceSize.height/2., faceSize.width - margin*2. - longTickHeight*2., faceSize.height - margin*2. - longTickHeight*2.), cornerRadius - margin - longTickHeight, cornerRadius - margin - longTickHeight, NULL);
 	
 	for (int i = 0; i < 12; i++)
 	{
         CGFloat angle = -(2*M_PI)/12.0 * i;
         
-        SKSpriteNode *tick = [SKSpriteNode spriteNodeWithColor:self.markColor size:CGSizeMake(2, workingRadius)];
+        CGAffineTransform rotation = CGAffineTransformMakeRotation(angle);
         
+        CGPoint lineLeftPointA = CGPointApplyAffineTransform(CGPointMake(-1, 0), rotation);
+        CGPoint lineLeftPointB = CGPointApplyAffineTransform(CGPointMake(-1, workingRadius), rotation);
+        CGPoint lineRightPointA = CGPointApplyAffineTransform(CGPointMake(1, 0), rotation);
+        CGPoint lineRightPointB = CGPointApplyAffineTransform(CGPointMake(1, workingRadius), rotation);
+        
+        CGPoint topLeft = intersectionBetweenPathAndLinePassingThroughPoints(outerPath, lineLeftPointA, lineLeftPointB);
+        CGPoint topRight = intersectionBetweenPathAndLinePassingThroughPoints(outerPath, lineRightPointA, lineRightPointB);
+        CGPoint bottomLeft = intersectionBetweenPathAndLinePassingThroughPoints(largeTickInnerPath, lineLeftPointA, lineLeftPointB);
+        CGPoint bottomRight = intersectionBetweenPathAndLinePassingThroughPoints(largeTickInnerPath, lineRightPointA, lineRightPointB);
+        
+        CGMutablePathRef tickPath = CGPathCreateMutable();
+        CGPathMoveToPoint(tickPath, NULL, topLeft.x, topLeft.y);
+        CGPathAddLineToPoint(tickPath, NULL, topRight.x, topRight.y);
+        CGPathAddLineToPoint(tickPath, NULL, bottomRight.x, bottomRight.y);
+        CGPathAddLineToPoint(tickPath, NULL, bottomLeft.x, bottomLeft.y);
+        CGPathCloseSubpath(tickPath);
+        
+        SKShapeNode *tick = [SKShapeNode shapeNodeWithPath:tickPath];
+        tick.fillColor = self.markColor;
+        tick.strokeColor = [SKColor clearColor];
         tick.position = CGPointZero;
-        tick.anchorPoint = CGPointMake(0.5, 1.5);
-        tick.zRotation = angle;
         
-        [majorTicksLayer addChild:tick];
+        [self addChild:tick];
 	}
+    
+    CGPathRelease(largeTickInnerPath);
 	
 	/* Minor */
-    SKCropNode *minorTicksLayer = [[SKCropNode alloc] init];
-    minorTicksLayer.position = CGPointZero;
-    minorTicksLayer.zPosition = 0;
     
-    SKShapeNode *minorTicksMask = [SKShapeNode shapeNodeWithRectOfSize:CGSizeMake(faceSize.width - margin*2. - shortTickHeight, faceSize.height - margin*2. - shortTickHeight) cornerRadius:cornerRadius - margin - shortTickHeight/2.];
-    minorTicksMask.lineWidth = shortTickHeight;
-    minorTicksMask.strokeColor = [SKColor colorWithWhite:1 alpha:1];
-    minorTicksMask.position = CGPointZero;
-    minorTicksMask.antialiased = NO;
+    CGPathRef smallTickInnerPath = CGPathCreateWithRoundedRect(CGRectMake(margin + shortTickHeight - faceSize.width/2., margin + shortTickHeight - faceSize.height/2., faceSize.width - margin*2. - shortTickHeight*2., faceSize.height - margin*2. - shortTickHeight*2.), cornerRadius - margin - shortTickHeight, cornerRadius - margin - shortTickHeight, NULL);
     
-    minorTicksLayer.maskNode = minorTicksMask;
-    [self addChild:minorTicksLayer];
+    for (int i = 0; i < 60; i++)
+    {
+        if (i % 5 == 0) continue;
+
+        CGFloat angle = - (2*M_PI)/60.0 * i;
+
+        CGAffineTransform rotation = CGAffineTransformMakeRotation(angle);
+
+        CGPoint lineLeftPointA = CGPointApplyAffineTransform(CGPointMake(-0.5, 0), rotation);
+        CGPoint lineLeftPointB = CGPointApplyAffineTransform(CGPointMake(-0.5, workingRadius), rotation);
+        CGPoint lineRightPointA = CGPointApplyAffineTransform(CGPointMake(0.5, 0), rotation);
+        CGPoint lineRightPointB = CGPointApplyAffineTransform(CGPointMake(0.5, workingRadius), rotation);
+
+        CGPoint topLeft = intersectionBetweenPathAndLinePassingThroughPoints(outerPath, lineLeftPointA, lineLeftPointB);
+        CGPoint topRight = intersectionBetweenPathAndLinePassingThroughPoints(outerPath, lineRightPointA, lineRightPointB);
+        CGPoint bottomLeft = intersectionBetweenPathAndLinePassingThroughPoints(smallTickInnerPath, lineLeftPointA, lineLeftPointB);
+        CGPoint bottomRight = intersectionBetweenPathAndLinePassingThroughPoints(smallTickInnerPath, lineRightPointA, lineRightPointB);
+
+        CGMutablePathRef tickPath = CGPathCreateMutable();
+        CGPathMoveToPoint(tickPath, NULL, topLeft.x, topLeft.y);
+        CGPathAddLineToPoint(tickPath, NULL, topRight.x, topRight.y);
+        CGPathAddLineToPoint(tickPath, NULL, bottomRight.x, bottomRight.y);
+        CGPathAddLineToPoint(tickPath, NULL, bottomLeft.x, bottomLeft.y);
+        CGPathCloseSubpath(tickPath);
+
+        SKShapeNode *tick = [SKShapeNode shapeNodeWithPath:tickPath];
+        tick.fillColor = self.markColor;
+        tick.strokeColor = [SKColor clearColor];
+        tick.position = CGPointZero;
+
+        [self addChild:tick];
+    }
     
-	for (int i = 0; i < 60; i++)
-	{
-		CGFloat angle = - (2*M_PI)/60.0 * i;
-		SKSpriteNode *tick = [SKSpriteNode spriteNodeWithColor:self.markColor size:CGSizeMake(1, workingRadius)];
-		
-		tick.position = CGPointZero;
-        tick.anchorPoint = CGPointMake(0.5, 1.5);
-		tick.zRotation = angle;
-		
-		if (i % 5 != 0)
-			[minorTicksLayer addChild:tick];
-	}
+    CGPathRelease(outerPath);
+    CGPathRelease(smallTickInnerPath);
 	
 	/* Numerals */
 	for (int i = 1; i <= 12; i++)
