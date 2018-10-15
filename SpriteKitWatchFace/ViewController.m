83cb274360a9e425ff44a8aeea2f73f7fcceb705
Steven Troughton-Smith <steventroughtonsmith@gmail.com>
2018-10-15 12:01:57 +0100

    Added new date display options. Adds ability to stack major tick marks on minor tickmarks. Better support for adding image backgrounds. Key to randomize settings & theme in desktop app: 'x', randomize using current theme: 'z'. Updates date on tick so should notice day changes.

--- SpriteKitWatchFace/ViewController.m
+++ SpriteKitWatchFace/ViewController.m
@@ -1,23 +1,37 @@
 //
 //  ViewController.m
-//  SpriteKitWatchFace
+//  DesktopShim
 //
-//  Created by Steven Troughton-Smith on 09/10/2018.
+//  Created by Steven Troughton-Smith on 10/10/2018.
 //  Copyright © 2018 Steven Troughton-Smith. All rights reserved.
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
-
-@end
+#import "FaceScene.h"
 
 @implementation ViewController
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+	[[NSUserDefaults standardUserDefaults] registerDefaults:@{@"Theme":@(ThemeMarques)}];
+	
+	FaceScene *scene = [FaceScene nodeWithFileNamed:@"FaceScene"];
+	
+	CGSize currentDeviceSize = [UIScreen mainScreen].bounds.size;
+	
+	CGFloat vertWidth = MIN(512, MIN(currentDeviceSize.width, currentDeviceSize.height));
+	
+	/* Using the 44mm Apple Watch as the base size, scale down to fit */
+	scene.camera.xScale = (184.0/vertWidth);
+	scene.camera.yScale = (184.0/vertWidth);
+	
+	[(SKView *)self.view presentScene:scene];
 }
 
+-(UIStatusBarStyle)preferredStatusBarStyle
+{
+	return UIStatusBarStyleLightContent;
+}
 
 @end
